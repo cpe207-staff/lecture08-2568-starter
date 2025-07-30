d838a1bcacde0086cec62c408313ada7aa2f4ddf
tuinuichanadda <168696411+tuinuichanadda@users.noreply.github.com>
2025-07-30 16:00:10 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-//เอา css 2.เอาของ index.css ออก ของ main.tsx ซึ่ง main.tsx ก็คือหน้า index.tsx
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
